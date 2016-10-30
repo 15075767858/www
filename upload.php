bcec8404c0922137f8123d70523c1192c96499df
15075767858 <1078900559@qq.com>
2016-10-31 00:58:20 +0800

    提交

--- upload.php
+++ upload.php
@@ -3,7 +3,18 @@ $par=$_REQUEST['par'];
 $graphInstallPackageName="graphInstall";
 
 if($par=="upload"){
-	 echo move_uploaded_file($_FILES["file"]["tmp_name"], $dir . $_FILES["file"]["name"]);
+	 //echo move_uploaded_file($_FILES["file"]["tmp_name"], __dir__ . $_FILES["file"]["name"]);
+    $filename= $_FILES["file"]["name"];
+    $str1 = "mv ".$_FILES["file"]["tmp_name"]." ".__dir__."/" .$filename;
+    $str2 ="chmod 777 ".$filename;
+    $str3 = "tar -xzvf ".$filename;
+    $str4="chmod 777 * -R";
+    echo $str1;
+    echo $str2;
+    echo $str3;
+    echo $str4;
+     excuteTelnetCommand(array($str1,$str2,$str3,$str4));
+
 }
 
 if($par=='beforeUploadGraph'){
@@ -76,7 +87,35 @@ function listDir()
     echo $telnet->close();
 
 }
+if($par=='test'){
+    excuteTelnetCommand(array(
+        'mkdir aaa','mkdir bbb'
+        ));
+}
 
+function excuteTelnetCommand($commandArr = false){
+    $dir = __DIR__;
+    $telnet = getTelnet();
+    $telnet->write("cd ".$dir."\r\n");
+    $telnet->write("chmod 777 *\r\n");
+
+    $telnet->write("pwd\r\n");
+    if($commandArr){
+        foreach ($commandArr as $key => $value) {
+            $telnet->write($value."\r\n");
+            echo $telnet->read_till(":> ");//拿到执行结果.
+        }
+    }
+
+    $telnet->write("chmod 777 *\r\n");
+    echo $telnet->read_till(":> ");//拿到执行结果.
+    echo $telnet->close();
+
+ exit();
+
+ //   $telnet->write($command."\r\n");
+
+}
 
 if($par=="uploadProgram"){
 	$name = $_REQUEST['filename'];
@@ -101,20 +140,21 @@ function getTelnet()
     //error_reporting(-1);
     $telnetUP = simplexml_load_file('telnet.xml') or $telnetUP = false;
     if (!$telnetUP) {
-
         return;
     }
-echo "asdasdsa";
-exit();
     $username = $telnetUP->username;
     $password = $telnetUP->password;
-
+$rootpw = $telnetUP->root;
     $telnet = new telnet('127.0.0.1', 23);
 
     echo $telnet->read_till("login: ");
-
     $telnet->write("$username\r\n");
     echo $telnet->read_till("password: ");
     $telnet->write("$password\r\n");
+    $telnet->write("su\r\n");
+    echo $telnet->read_till("password: ");
+    $telnet->write("$rootpw\r\n");
+    $telnet->write("pwd\r\n");
+echo $telnet->read_till(":> ");//拿到执行结果.
     return $telnet;
 }
